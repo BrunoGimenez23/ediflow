e333f15b47ca59439c93ffb7e8ace96b28fb64ea
Bruno Gimenez <brunogimenez23@hotmail.com>
2025-04-18 11:21:20 -0300

    Imicualizacion del proyecto Ediflow

---
+++ backend/src/main/java/com/ediflow/backend/EdiflowApplication.java
@@ -0,0 +1,13 @@
+package com.ediflow.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdiflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdiflowApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ediflow/backend/EdiflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ediflow.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdiflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
