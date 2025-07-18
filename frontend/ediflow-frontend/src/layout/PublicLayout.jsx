314ff16cdd1e2373a4bcbc8ab0c37529b0dc3106
Bruno Gimenez <brunogimenez23@hotmail.com>
2025-07-18 18:43:24 -0300

    Agregando navbar a lolgin y register

--- frontend/ediflow-frontend/src/layout/PublicLayout.jsx
+++ frontend/ediflow-frontend/src/layout/PublicLayout.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from '../components/landing/Navbar';
+import { Outlet } from 'react-router-dom';
 
 const PublicLayout = ({ children }) => {
   return (
     <>
       <Navbar />
-      <main className="p-6 max-w-7xl mx-auto">{children}</main>
+      <main className="p-6 max-w-7xl mx-auto"><Outlet /></main>
     </>
   );
 };
