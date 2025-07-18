0981d42cbcc8b8a8ce61c0486be9feb5333fbfed
Bruno Gimenez <brunogimenez23@hotmail.com>
2025-07-18 18:46:59 -0300

    Agregando navbar a lolgin y register

--- frontend/ediflow-frontend/src/App.jsx
+++ frontend/ediflow-frontend/src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 import AdminDashboard from './pages/admin/AdminDashboard'
 import Buildings from './pages/admin/Buildings'
 import Layout from './layout/Layout'
-import PublicLayout from './layout/PublicLayout' 
+import PublicLayout from './layout/PublicLayout'
 import Register from './pages/auth/Register'
 import Home from './pages/home/Home'
 import Login from './pages/auth/Login'
@@ -24,14 +24,16 @@ import PlanConfirmationPage from './pages/admin/PlanConfirmationPage'
 function App() {
   return (
     <Routes>
-      {/* Rutas públicas con PublicLayout */}
+      {/* Home directo, sin layout */}
+      <Route path='/' element={<Home />} />
+
+      {/* Login y Register con PublicLayout */}
       <Route element={<PublicLayout />}>
-        <Route path='/' element={<Home />} />
         <Route path="/auth/register-admin" element={<Register />} />
         <Route path="/auth/login" element={<Login />} />
       </Route>
 
-      {/* Rutas de administrador con Layout (Sidebar + Header) */}
+      {/* Rutas de administrador con Layout */}
       <Route path='/admin' element={<Layout />}>
         <Route index element={<AdminDashboard />} />
         <Route path="reservas" element={<AdminReservationsPanel />} />
@@ -47,7 +49,7 @@ function App() {
       
       <Route path="planes/confirmacion/:planName" element={<PlanConfirmationPage />} />
 
-      {/* Rutas de residente sin layout o con otro layout si querés */}
+      {/* Rutas de residente */}
       <Route path='/resident' element={<ResidentDashboard />} />
       <Route path="/mis-pagos" element={<MyPayments />} />
       <Route path="/mis-reservas" element={<MyReservations />} />
