d85212890ad8c84af17be7fdc53801d36c987d4a
Bruno Gimenez <brunogimenez23@hotmail.com>
2025-07-18 12:15:04 -0300

    Arreglando problema de cors

--- backend/src/main/java/com/ediflow/backend/configuration/SecurityConfiguration.java
+++ backend/src/main/java/com/ediflow/backend/configuration/SecurityConfiguration.java
@@ -33,7 +33,7 @@ public class SecurityConfiguration {
         return http
                 .cors(cors -> cors.configurationSource(request -> {
                     var corsConfig = new org.springframework.web.cors.CorsConfiguration();
-                    corsConfig.setAllowedOrigins(java.util.List.of("http://localhost:5173"));
+                    corsConfig.setAllowedOrigins(java.util.List.of("http://localhost:5173", "https://ediflow23.vercel.app"));
                     corsConfig.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     corsConfig.setAllowedHeaders(java.util.List.of("*"));
                     return corsConfig;
